9277603b7fe6b422d2eb22966ceb116914036ba0
unknown <WilliamNdongmo88>
2024-08-21 13:55:53 +0200

    java/class_logiques

--- src/com/marchant/ParkingBot.java
+++ src/com/marchant/ParkingBot.java
@@ -1,8 +1,21 @@
 package com.marchant;
 
+import com.marchant.model.Ticket;
+import com.marchant.model.Vehicule;
+import service.ParkingService;
+
 public class ParkingBot {
 
     public static void main(String[] args) {
+        ProcessVehicule();
+    }
 
+    private static void ProcessVehicule() {
+        Vehicule vehicule = new Vehicule();
+        vehicule.setRegistrationNumber("LS-123-PM");
+        vehicule.setCategory("CITADINE");
+        ParkingService parkingService = new ParkingService();
+        Ticket ticket = parkingService.traiterVehiculeEntrant(vehicule);
+        System.out.println("Ticket : " + ticket);
     }
 }
--- src/service/ParkingService.java
+++ src/service/ParkingService.java
@@ -1,6 +1,16 @@
 package service;
 
+import com.marchant.model.Ticket;
+import com.marchant.model.Vehicule;
+
+import java.util.Date;
+
 public class ParkingService {
-    public String parking = " Parking et surveillance de véhicules";
-    public String washing = " Nettoyage de véhicules";
+    public Ticket traiterVehiculeEntrant(Vehicule vehicule){
+        Ticket ticket = new Ticket();
+        Date now = new Date();
+        ticket.setInteringTime(now);
+        ticket.setVehicule(vehicule);
+        return ticket;
+    }
 }
