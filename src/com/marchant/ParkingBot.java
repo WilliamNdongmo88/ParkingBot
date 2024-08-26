e281caaa834931ea40b2ffc891eb959d187aacf2
unknown <WilliamNdongmo88>
2024-08-26 14:42:15 +0200

    Java Dictionaire/Map

--- src/com/marchant/ParkingBot.java
+++ src/com/marchant/ParkingBot.java
@@ -6,6 +6,11 @@ import com.marchant.model.Ticket;
 import com.marchant.model.Vehicule;
 import service.ParkingService;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class ParkingBot {
     static ParkingService parkingService = new ParkingService();
 
@@ -14,22 +19,34 @@ public class ParkingBot {
     }
 
     private static void ProcessVehicule() {
-        Ticket[] tabTicket = new Ticket[3];
+        ArrayList<Ticket> ticketList = new ArrayList<>();
+        Map<Integer,Ticket> ticketMap = new HashMap<>();
+        Integer position = 0;
+
         Vehicule vehicule = new Vehicule();
         vehicule.setRegistrationNumber("LS-458-P1");
-        tabTicket[0] = parkingService.traiterVehiculeEntrant(vehicule);
+        Ticket ticketVehicule = parkingService.traiterVehiculeEntrant(vehicule);
+        ticketList.add(ticketVehicule);
 
         Vehicule voiture = new Car();
         voiture.setRegistrationNumber("LT-879-P2");
-        tabTicket[1] = parkingService.traiterVehiculeEntrant(voiture);
+        Ticket ticketVoiture = parkingService.traiterVehiculeEntrant(voiture);
+        ticketList.add(ticketVoiture);
 
         Vehicule threecycle = new Bicycle();
         threecycle.setRegistrationNumber("OU-589-P3");
-        tabTicket[2] = parkingService.traiterVehiculeEntrant(threecycle);
+        Ticket ticketThreecycle = parkingService.traiterVehiculeEntrant(threecycle);
+        ticketList.add(ticketThreecycle);
 
-        System.out.println("Triatement des sorties par lot");
-        for (int i = 0; i < tabTicket.length; i++) {
-            System.out.println(parkingService.traiterVehiculeEntrant(tabTicket[i].getVehicule()));
+        System.out.println("Dictionnaire/Map Triatement des sorties par lot");
+        for (Ticket ticket : ticketList) {
+            System.out.println(parkingService.traiterVehiculeEntrant(ticket.getVehicule()));
+            ticketMap.put(position, ticket);
+            position ++;
+        }
+        Set<Integer> keySet = ticketMap.keySet();
+        for(Integer key : keySet){
+            System.out.println("Position dans la liste "+ key + " : "+ ticketMap.get(key).getVehicule().getCategory());
         }
     }
 }
